090f8008dd10ce74a113ed6a520618aa11827427
js <54337467@qq.com>
2021-04-07 17:03:16 +0800

    登录布局

---
+++ src/views/Login.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="login-wrap">
+    <el-form
+      :model="formData"
+      class="login-form"
+      label-position="top"
+      label-width="80px"
+      >
+      <h2>用户登录</h2>
+      <el-form-item label="用户名">
+        <el-input v-model="formData.username" clearable></el-input>
+      </el-form-item>
+      <el-form-item label="密码">
+        <el-input v-model="formData.password" show-password clearable></el-input>
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary" class="login-btn">登 录</el-button>
+      </el-form-item>
+    </el-form>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      formData: {
+        username: '',
+        password: ''
+      }
+    }
+  }
+}
+</script>
+
+<style lang="less">
+.login-wrap {
+  background-color: #324152;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  .login-form {
+    background-color: #fff;
+    width: 400px;
+    padding: 30px;
+    border-radius: 5px;
+
+    .login-btn {
+      width: 100%;
+    }
+  }
+}
+</style>
