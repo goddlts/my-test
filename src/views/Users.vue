5c59d1024ec103559a1e8aaf01c6f1211f7e17e7
js <54337467@qq.com>
2021-04-09 15:12:30 +0800

    用户列表-头部

--- src/views/Users.vue
+++ src/views/Users.vue
@@ -1,15 +1,34 @@
 <template>
-  <div>
-    Users
-  </div>
+  <el-card class="card">
+    <el-breadcrumb separator-class="el-icon-arrow-right">
+      <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
+      <el-breadcrumb-item>用户管理</el-breadcrumb-item>
+      <el-breadcrumb-item>用户列表</el-breadcrumb-item>
+    </el-breadcrumb>
+    <!-- 搜索框 -->
+    <div style="margin-top: 10px;">
+      <!-- 搜索框 -->
+      <el-input style="width: 300px;" placeholder="请输入内容" v-model="searchValue">
+        <el-button slot="append" icon="el-icon-search"></el-button>
+      </el-input>
+
+      <el-button type="success" plain style="margin-left: 10px;">添加用户</el-button>
+    </div>
+  </el-card>
 </template>
 
 <script>
 export default {
-
+  data () {
+    return {
+      searchValue: ''
+    }
+  }
 }
 </script>
 
-<style>
-
+<style lang="less">
+.card {
+  height: 100%;
+}
 </style>
