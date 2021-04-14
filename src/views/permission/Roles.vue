b3b88825b65831ef24464f89f52e43f626f4b903
js <54337467@qq.com>
2021-04-14 15:25:56 +0800

    展开权限

--- src/views/permission/Roles.vue
+++ src/views/permission/Roles.vue
@@ -13,6 +13,31 @@
       style="width: 100%">
       <el-table-column
         type="expand">
+        <template slot-scope="scope">
+          <!-- 展示一级权限 -->
+          <el-row v-for="level1 in scope.row.children" :key="level1.id">
+            <el-col :span="4">
+              <el-tag class="tag" closable>{{ level1.authName }}</el-tag> >
+            </el-col>
+            <el-col :span="20">
+              <!-- 展示二级权限 -->
+              <el-row v-for="level2 in level1.children" :key="level2.id">
+                <el-col :span="5">
+                  <el-tag class="tag" type="success" closable>{{ level2.authName }}</el-tag> >
+                </el-col>
+                <el-col :span="19">
+                  <!-- 展示三级权限 -->
+                  <el-tag
+                    class="tag"
+                    v-for="level3 in level2.children"
+                    :key="level3.id"
+                    closable
+                    >{{ level3.authName }}</el-tag>
+                </el-col>
+              </el-row>
+            </el-col>
+          </el-row>
+        </template>
       </el-table-column>
       <el-table-column
         prop="roleName"
@@ -55,6 +80,8 @@ export default {
 }
 </script>
 
-<style>
-
+<style lang="less">
+.tag {
+  margin: 5px 5px;
+}
 </style>
