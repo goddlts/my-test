1783f5143973a5245fc2daa9db8b7be0d1fd22a4
js <54337467@qq.com>
2021-04-21 14:42:15 +0800

    步骤条

--- src/views/goods/GoodsAdd.vue
+++ src/views/goods/GoodsAdd.vue
@@ -9,17 +9,32 @@
       :closable="false"
       show-icon>
     </el-alert>
+    <!-- 步骤条 -->
+    <el-steps :active="active" align-center finish-status="success">
+      <el-step title="基本信息"></el-step>
+      <el-step title="商品参数"></el-step>
+      <el-step title="商品属性"></el-step>
+      <el-step title="商品图片"></el-step>
+      <el-step title="商品描述"></el-step>
+    </el-steps>
   </el-card>
 </template>
 
 <script>
 import MyComponents from '../../components/MyComponents.vue'
 export default {
-  components: { MyComponents }
-
+  components: { MyComponents },
+  data () {
+    return {
+      active: 0
+    }
+  }
 }
 </script>
 
 <style>
-
+.el-step__title {
+  font-size: 12px;
+  line-height: 38px;
+}
 </style>
